4ee2e5a6547d1d04fbd4daed0a5a25a2158cf6b4
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2015-03-20 21:22:14 -0400

    Add empty ChatViewController and ChatMessageCell classes

---
+++ LayerQuickStart/ChatMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageCell.swift
+//  LayerQuickStart
+//
+//  Created by Jawwad Ahmad on 3/20/15.
+//  Copyright (c) 2015 Layer. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UITableViewCell {
+
+}
